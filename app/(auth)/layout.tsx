00ad2d81ba0326320636982088c869d4fb18eb4a
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2024-08-01 13:22:40 +0545

    clerk setup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,9 @@
 import React, { ReactNode } from "react";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <div className="h-full">{children}</div>;
+  return (
+    <div className="h-full flex justify-center items-center">{children}</div>
+  );
 };
 
 export default AuthLayout;
