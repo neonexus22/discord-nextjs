08939b76109dde61e2bf72ebcd9925708e8c8572
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2024-08-01 17:03:07 +0545

    1. installed dialog, input and form 2. created initial modal 3. added dialog initials

---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+} from "@/components/ui/dialog";
+
+const InitialModal = () => {
+  return (
+    <Dialog open>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Customize your server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Give your server a personality with a name and an image. You can
+            always change it later.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default InitialModal;
