f6e01ef2e21f4fc0cadd6e6fe05d13bf3b53ab2d
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2024-08-02 22:12:12 +0545

    server click action 1. added modeToggle 2. added userbutton

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -15,9 +15,13 @@ const NavigationItem = ({ id, name, imageUrl }: Props) => {
   const params = useParams();
   const router = useRouter();
 
+  const onClick = () => {
+    router.push(`/server/${id}`);
+  };
+
   return (
     <ActionTooltip side="right" align="center" label={name}>
-      <button onClick={() => {}} className="group relative flex items-center">
+      <button onClick={onClick} className="group relative flex items-center">
         <div
           className={cn(
             "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
