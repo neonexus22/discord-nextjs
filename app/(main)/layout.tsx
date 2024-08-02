740f12621ece5e53965c9905dd9004f7014f1b1f
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2024-08-02 13:42:04 +0545

    navigation sidebar 1. navigation-sidebar style 2. get all servers for profile 3. navigation-action child style 3. install tooltip/separator

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import NavigationSidebar from "@/components/navigation/navigation-sidebar";
 import { ReactNode } from "react";
 
 type Props = {
@@ -5,7 +6,14 @@ type Props = {
 };
 
 const MainLayout = async ({ children }: Props) => {
-  return <div>{children}</div>;
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+        <NavigationSidebar />
+      </div>
+      <main className="md:pl-[72px] h-full">{children}</main>
+    </div>
+  );
 };
 
 export default MainLayout;
