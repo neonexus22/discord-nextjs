fac0dacd322a0652f24b9d46bc39b338cc638487
nishant-spendflo <112845053+nishant-spendflo@users.noreply.github.com>
2024-08-06 13:34:21 +0545

    added key event for search cmd

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Search } from "lucide-react";
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import {
   CommandDialog,
   CommandEmpty,
@@ -27,6 +27,19 @@ type Props = {
 
 const ServerSearch = ({ data }: Props) => {
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => {
+      document.removeEventListener("keydown", down);
+    };
+  }, []);
 
   return (
     <>
