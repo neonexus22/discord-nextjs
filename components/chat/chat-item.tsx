da788614318f5069997b0659da9a171f7f5b4d76
nishant-shah <112845053+nishant-spendflo@users.noreply.github.com>
2024-08-10 14:20:41 +0545

    chat item content and actions 1. show message and (edited) if updated 2. show edit and delete button on hover of message

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -1,9 +1,10 @@
 import { Member, MemberRole, Profile } from "@prisma/client";
 import UserAvatar from "@/components/user-avatar";
 import ActionTooltip from "@/components/action-tooltip";
-import { FileIcon, ShieldAlert, ShieldCheck } from "lucide-react";
+import { Edit, FileIcon, ShieldAlert, ShieldCheck, Trash } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 type Props = {
   id: string;
@@ -40,6 +41,9 @@ const ChatItem = ({
   socketUrl,
   socketQuery,
 }: Props) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
   const fileType = fileUrl?.split(".").pop();
 
   const isAdmin = currentMember.role === MemberRole.ADMIN;
@@ -102,8 +106,38 @@ const ChatItem = ({
               </a>
             </div>
           )}
+          {!fileUrl && !isEditing && (
+            <p
+              className={cn(
+                "text-sm text-zinc-600 dark:text-zinc-300",
+                deleted &&
+                  "italic text-zinc-500 dark:text-zinc-400 text-xs mt-1"
+              )}
+            >
+              {content}
+              {isUpdated && !deleted && (
+                <span className="text-[10px] text-zinc-500 dark:text-zinc-400">
+                  (edited)
+                </span>
+              )}
+            </p>
+          )}
         </div>
       </div>
+      {canDeleteMessage && (
+        <div className="hidden group-hover:flex items-center gap-x-2 absolute p-1 -top-2 right-5 bg-white dark:bg-zinc-800 border rounded-sm">
+          {canEditMessage && (
+            <ActionTooltip label="Edit">
+              <Edit className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transistion" />
+            </ActionTooltip>
+          )}
+          {canDeleteMessage && (
+            <ActionTooltip label="Delete">
+              <Trash className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transistion" />
+            </ActionTooltip>
+          )}
+        </div>
+      )}
     </div>
   );
 };
